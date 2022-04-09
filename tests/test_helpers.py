f1e37d45b3cfebbc18b95426fd36dc5d0e98ba42
Matt Burnside <burnside.matthew@gmail.com>
2022-04-09 04:33:45 -0700

    Update project name and add boredle class and tests (#4)
    * add tests to test_helpers
    
    * add .vscode directory to gitignore
    
    * rename project directories

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import pytest
 from english_words import english_words_lower_alpha_set as wordlist
 from wordle.helpers import (
     exclude_by_char,
@@ -13,3 +14,21 @@ from wordle.helpers import (
 def test_wordlist():
     words = prep_wordlist(wordlist)
     assert all([len(word) == 5 for word in words])
+
+
+@pytest.mark.parametrize("chars, wordlist", [("hae", ["hear", "stear", "earth"])])
+def test_include_by_char(chars, wordlist):
+    words = include_by_char(chars, wordlist)
+    assert len(words) == 2
+    assert "hear" in words and "earth" in words
+
+
+@pytest.mark.parametrize(
+    "chars, wordlist", [("pn", ["pander", "elate", "loose", "ponsi", "store"])]
+)
+def test_exclude_by_char(chars, wordlist):
+    words = exclude_by_char(chars, wordlist)
+    assert len(words) == 3
+    assert "elate" in words
+    assert "loose" in words
+    assert "store" in words
