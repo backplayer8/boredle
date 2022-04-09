92d7039eac186a04ae6098e0e5256bb21b7680c9
Matt Burnside <burnside.matthew@gmail.com>
2022-04-09 00:30:05 -0700

    Create Boredle class and add tie in helpers

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 ## A small collection of tools to help solve the daily puzzle by [Wordle](https://www.powerlanguage.co.uk/wordle/ "Guess the Wordle in 6 tries")
 
-This is just a few functions that I wrote in order to finally succeed at a word game. At this time these functions are standalone and have no defined order or flow. At its inception, these functions are all being ran from REPL as I progress. The  My goal for this repo is to take it from a few standalone functions that are ran in a semi-random order to a structured tool, optemized to give the best possible chance of solving the task in as few turns as possible. I welcome any feedback or PR, this is my first public repo, please be gentle, or don't, I'm not your momma. If you've read this far, thanks for your time and I hope at least one other person finds this repo helpful.
-
-Copyright 2022 Matt Burnside <matt_codes@outlook.com>
+This started off as just a few functions that I wrote in order to finally succeed at a word game. At this stage of the script I have been running this in repl though it's at a point it can be scripted easily enough. It still has a ways to go and I'm sure tons of room for improvement but it has been a fun little project. I welcome any feedback or PR, this is my first public repo, please be gentle, or don't, I'm not your momma. If you've read this far, thanks for your time and I hope at least one other person finds this repo fun/helpful.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
--- boredle/boredle.py
+++ boredle/boredle.py
@@ -1,24 +1,48 @@
+from copy import copy
+from tkinter import N
 from english_words import english_words_lower_alpha_set as wordlist
 from secrets import choice
+from typing import Union
 
 
 class Boredle(object):
-    def __init__(self):
+  
+    def __init__(
+        self,
+        first_guess: Union[str, None]=None,
+        second_guess: Union[str, None]=None,
+        third_guess: Union[str, None]=None,
+        fourth_guess: Union[str, None]=None,
+        fifth_guess: Union[str, None]=None,
+        sixth_guess: Union[str, None]=None,
+    ):
         self._wordlist = [word for word in wordlist if len(word) == 5]
-        self.first_guess = None
-        self.second_guess = None
-        self.third_guess = None
-        self.fourth_guess = None
-        self.fifth_guess = None
-        self.sixth_guess = None
-        self.first_guess = self.guess()
-        self._correct = {}
+        self.first_guess = self.guess() if first_guess is None else first_guess
+        self.second_guess = second_guess
+        self.third_guess = third_guess
+        self.fourth_guess = fourth_guess
+        self.fifth_guess = fifth_guess
+        self.sixth_guess = sixth_guess
+        self._correct_letters = {
+            0: None,
+            1: None,
+            2: None, 
+            3: None,
+            4: None,
+        }
         print(f"Suggested first guess is: {self.first_guess}")
 
     def __repr__(self) -> str:
-        return f"""{__class__.__name__}(First_Guess: {self.first_guess}, Second_Guess: {self.second_guess}, Third_Guess: {self.third_guess}, Fourth_Guess: {self.fourth_guess}, Fifth_Guess: {self. fifth_guess}, sixth_guess: {self.sixth_guess}"""
+        return f"""{__class__.__name__}(
+            first_guess="{self.first_guess}",
+            second_guess="{self.second_guess}",
+            third_guess="{self.third_guess}",
+            fourth_guess="{self.fourth_guess}",
+            fifth_guess="{self. fifth_guess}",
+            sixth_guess="{self.sixth_guess}"
+            )"""
 
-    def guess(self):
+    def guess(self, _guess:Union[str, None]=None) -> str:
         match None:
             case self.first_guess:
                 self.first_guess = choice(self._wordlist)
@@ -42,57 +66,127 @@ class Boredle(object):
             case _:
                 print("Game Over!")
 
+    def educated_guess(self, *chars):
+        words = copy(self._wordlist)
+        common_words = []
+        for char in chars:
+            common_words.extend([word for word in words if char in word])
+        return common_words
+
+    def _guess(self):
+        words = copy(self._wordlist)
+        word = choice(words)
+        if all([False for char in word if word.count(char) != 1]):
+            words.remove(word)
+            return word
+        else:
+            return self.guess()
+
+    def redo_guess(self, _guess):
+        if _guess is not None:
+            pass
+
+    def redo_guess(self, criteria=None, guess=None):
+        match type(str):
+            case self.sixth_guess:
+                self.sixth_guess = None
+            case self.fifth_guess:
+                self.fifth_guess = None
+            case self.fourth_guess:
+                self.fourth_guess = None
+            case self.third_guess:
+                self.third_guess = None
+            case self.second_guess:
+                self.second_guess = None
+            case self.first_guess:
+                self.first_guess = None
+            case _:
+                raise ValueError("criteria must be None or valid state for a string")
+        
+
     @property
     def words(self):
         return self._wordlist
 
-    def prep_wordlist(self, size: int = 5):
+    def _prep_wordlist(self, size: int = 5):
         return [word for word in self._wordlist if len(word) == size]
 
-    def exclude_by_char(self, chars):
+    def exclude_by_char(self, chars:Union[str,list[str]]) -> None:
+        """exclude_by_char is to be called when you have characters not in
+        the word at all. e.g. the letter inside a black box. Removes words
+        with the incorrect letters.
+
+        Args:
+            chars (str): The characters to exclude. Should not contain any
+            characters except a-z.
+        """
         self._wordlist = [
             word for word in self._wordlist if all(char not in word for char in chars)
         ]
 
-    def include_by_char(self, chars):
+    def include_by_char(self, chars:Union[str,list[str]]) -> None:
+        """include_by_char is to be called when you have characters in
+        the word either in the correct or incorrect index is fine. e.g.
+        the letter inside a black box. Removes words from the list that
+        do not contain the letters passed to the function.
+
+        Args:
+            chars (_type_): _description_
+        """
         self._wordlist = [
             word for word in self._wordlist if all(char in word for char in chars)
         ]
 
     @property
-    def correct_characters(self):
-        return self._correct
+    def correct_letters(self):
+        return self._correct_letters
 
-    @correct_characters.setter
+    @correct_letters.setter
     def correct_characters(self, chars_idxs):
         try:
             chars, indexes = chars_idxs
         except ValueError as err:
-            raise(ValueError(err, 'Accepts a tuple like: ("chars", [0, 1, 2, 3, 4])'))
+            raise (ValueError(err, 'Accepts a tuple like: ("chars", [0, 1, 2, 3, 4])'))
         else:
             if len(chars) != len(indexes):
-                raise(ValueError("string length must be the same as number of indexes"))
+                raise (
+                    ValueError("string length must be the same as number of indexes")
+                )
 
-            self._correct.update({index:char for char, index in zip(chars, indexes)})
-        
+            self._correct_letters.update({index: char for char, index in zip(chars, indexes)})
 
-    def lock_in_index(self, characters, indices):
+    def count_remaining_words(self):
+        return len(self._wordlist)
 
+    def lock_in_index(self, characters: str, indices: Union[str, list[int]]):
         if len(characters) != len(indices):
-
             raise IndexError("Number of characters should equal number of indexes")
+
+        if isinstance(indices, str):
+            indices = [int(num) for num in indices]
+
         self._wordlist = [
             word
             for word in self._wordlist
             if all(word[idx] == char for char, idx in zip(characters, indices))
         ]
+        self.correct_characters = (characters, indices)
 
-    def lock_out_index(self, char, indices):
+    def lock_out_index(self, char: str, indices: Union[str, list[int]]):
         if len(char) != len(indices):
             raise IndexError("Number of characters should equal number of indexes")
+
+        if isinstance(indices, str):
+            indices = [int(num) for num in indices]
 
         self._wordlist = [
             word
             for word in self._wordlist
             if all(word[idx] != ch for ch, idx in zip(char, indices))
         ]
+
+    def filter_multiples(self, char: str, repeats: int):
+        self._wordlist = [
+            word for word in self._wordlist if word.count(char) == repeats
+        ]
+        
